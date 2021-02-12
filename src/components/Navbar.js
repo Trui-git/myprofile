97b0039aad2aa3d8b2b6b1607571bb4b3708a907
ruiteng2021 <tengrui13@gmail.com>
2021-02-12 13:54:21 -0500

    open new window

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,14 @@ const Navigation = (props) => {
     console.log(props);
     return (
         <Navbar>
-            <Navbar.Brand href="#/">
-                <div className="icon"></div>
-            </Navbar.Brand>
-                <Nav className="mr-auto">                               
-                    <Nav.Link className="navset" href="#/"></Nav.Link>
-                    {/*
-                    <Nav.Link className="btn-space" href="#/Works">Works</Nav.Link>
-                    <Nav.Link className="btn-space" href="#/Aquarium">Aquarium</Nav.Link>
-                    <Nav.Link className="btn-space" href="#/About">About</Nav.Link>
-                    */}
-                </Nav>
+            <Nav className="mr-auto">                               
+                <Nav.Link className="navset" href="#/"></Nav.Link>
+                {/*
+                <Nav.Link className="btn-space" href="#/Works">Works</Nav.Link>
+                <Nav.Link className="btn-space" href="#/Aquarium">Aquarium</Nav.Link>
+                <Nav.Link className="btn-space" href="#/About">About</Nav.Link>
+                */}
+            </Nav>
         </Navbar>
     )
 }
