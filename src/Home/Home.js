9a7d1d42b5ad13e1afe794d72b682eb9a42159e3
ruiteng2021 <tengrui13@gmail.com>
2021-06-26 21:19:11 -0400

    finish all courses

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -106,13 +106,13 @@ export default class Home extends Component {
                                 aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}></div>
                   </div>
                 <strong>Advanced Web technologies</strong>
-                <span className="float-right">On Going</span>
+                <span className="float-right">Done</span>
                   <div className="progress">
                     <div className="progress-bar progress-bar-primary" role="progressbar" 
                                 aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}></div>
                   </div>
                   <strong>Linux and Apache</strong>
-                <span className="float-right">On Going</span>
+                <span className="float-right">Done</span>
                   <div className="progress">
                     <div className="progress-bar progress-bar-primary" role="progressbar" 
                                 aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}></div>
